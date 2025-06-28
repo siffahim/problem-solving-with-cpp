a935a4ffe256be319efb9225348cf43a73b2eb51
SAIFUL ISLAM (Fahim) <saifulislamfahim.me@gmail.com>
2025-06-28 17:07:19 +0600

    slove 4 problem

--- Sorted.cpp
+++ Sorted.cpp
@@ -2,22 +2,23 @@
 using namespace std;
 
 int main(){
-    int n;
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
     cin >> n;
-    vector<int> v(n);
+    int a[n];
+
     for (int i = 0; i < n; i++){
-        cin >> v[i];
+        cin >> a[i];
     }
 
-    vector<int> v2;
-    int flag = 0;
+    int flag = 1;
     for (int i = 0; i < n; i++){
-        vector<int>::iterator it = find(v2.begin(), v2.end(), v[i]);
-        if(it == v2.end()){
-            v2.push_back(v[i]);
-        }
-        else{
-            flag = 1;
+        if(a[i] < a[i - 1]){
+            flag = 0;
+            break;
         }
     }
 
@@ -27,6 +28,7 @@ int main(){
     else{
         cout << "NO" << endl;
     }
-    
+    }
+
     return 0;
 }
